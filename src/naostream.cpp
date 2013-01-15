85ebfe85b6323043a52b0a0853b0ec6b5c189def
Steven Laan <therealneojume@gmail.com>
2013-01-15 17:00:35 +0100

    Added feature normalization

--- src/naostream.cpp
+++ src/naostream.cpp
@@ -12,7 +12,7 @@
 #include <alerror/alerror.h>
 #include <alproxies/almotionproxy.h>
 
-// Opencv includes.
+// OpenCV includes.
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -80,43 +80,98 @@ int main( int argc, char* argv[] ) {
 		double min_dist = 100;
 
 		// Quick calculation of max and min distances between keypoints
-		for( int i = 0; i < current_descriptors.rows; i++ ) { 
+		for ( int i = 0; i < current_descriptors.rows; i++ ) { 
 			double dist = matches[i].distance;
-			if( dist < min_dist ) min_dist = dist;
-			if( dist > max_dist ) max_dist = dist;
+			if ( dist < min_dist ) min_dist = dist;
+			if ( dist > max_dist ) max_dist = dist;
 		}
 
-		// Find the good matches (i.e. whose distance is less than 2*min_dist )
+		// Find the good matches and calculate centroids
 		std::vector<cv::DMatch> good_matches;
-		std::vector<cv::Point2f> current_points, previous_points;
+
+		cv::Point2f current_centroid( 0, 0 );
+		cv::Point2f previous_centroid( 0, 0 );
 
 		for ( int i = 0; i < current_descriptors.rows; i++ ) { 
 			if ( matches[i].distance < 2 * min_dist ) {
-				current_points.push_back( current_keypoints[matches[i].queryIdx].pt );
-				previous_points.push_back( previous_keypoints[matches[i].trainIdx].pt );
+				current_centroid  += current_keypoints[matches[i].queryIdx].pt;
+				previous_centroid += previous_keypoints[matches[i].trainIdx].pt;
 
-				good_matches.push_back( matches[i] ); 
+				good_matches.push_back( matches[i] );
 			}
 		}
 
+		// Normalize the centroids
+		current_centroid.x /= matches.size();
+		current_centroid.y /= matches.size();
+
+		previous_centroid.x /= matches.size();
+		previous_centroid.y /= matches.size();
+
+		double current_scaling = 0;
+		double previous_scaling = 0;
+		std::vector<cv::DMatch>::iterator i;
+
+		for ( i = good_matches.begin(); i != good_matches.end(); i++ ) {
+			cv::Point2f * cp = &current_keypoints[i->queryIdx].pt;
+			cv::Point2f * pp = &previous_keypoints[i->trainIdx].pt;
+
+			*cp -= current_centroid;
+			*pp -= previous_centroid;
+
+			current_scaling += sqrt( cp->dot( *cp ) );
+			previous_scaling += sqrt( pp->dot( *pp ) );
+		}
+
+		// Enforce mean distance sqrt( 2 ) from origin
+		current_scaling  = sqrt( 2.0 ) * (double) good_matches.size() / current_scaling;
+		previous_scaling = sqrt( 2.0 ) * (double) good_matches.size() / previous_scaling;
+
+		// Scale features and store the points
+		std::vector<cv::Point2f> current_points, previous_points;
+
+		for ( i = good_matches.begin(); i != good_matches.end(); i++ ) {
+			current_points.push_back( current_keypoints[i->queryIdx].pt * current_scaling );
+			previous_points.push_back( previous_keypoints[i->trainIdx].pt * previous_scaling );
+		}
+
+		// Compute transformation matrices
+		cv::Mat current_T, previous_T;
+		current_T = ( cv::Mat_<double>(3,3) << 
+			current_scaling, 0,               -current_scaling * current_centroid.x, 
+			0,               current_scaling, -current_scaling * current_centroid.y, 
+			0,               0,               1 
+		);
+
+		previous_T = ( cv::Mat_<double>(3,3) << 
+			previous_scaling, 0,                -previous_scaling * previous_centroid.x, 
+			0,                previous_scaling, -previous_scaling * previous_centroid.y, 
+			0,                0,                1 
+		);
+
 		// Compute fundamental matrix
 		cv::Mat F = cv::findFundamentalMat( previous_points, current_points );
-
-		//std::cout << F << std::endl;
+		F = current_T.t() * F * previous_T;
 
 		// Compute essential matrix
-		cv::Mat E,U,S,V,T,Ra,Rb,W,Z;
+		cv::Mat E, U, S, V, T, Ra, Rb, W, Z;
 		E = K.inv().t() * F * K;
+
+		cv::Mat U, W, V;
+		cv::SVD::compute( E, U, W, V );
+		W.at<double>( 2, 0 ) = 0;
+		E = U * W.diag() * V.t(); // Possible transpose error
 
 		// Hartley matrices
 		W = ( cv::Mat_<double>(3,3) << 0,-1, 0, 1, 0, 0, 0, 0, 1 );
 		Z = ( cv::Mat_<double>(3,3) << 0, 1, 0,-1, 0, 0, 0, 0, 0 );
 
-		cv::SVD.compute( E, U, S, V );
+		// Compute R and T
+		cv::SVD::compute( E, U, S, V );
 		T = U * Z * U.t();
-		Ra = U * W * V.t();
-		Rb = U * W.t()*V.t();
-		
+		Ra = U * W * V.t(); // Possible transpose error
+		Rb = U * W.t() * V.t(); // Possible transpose error
+
 		cv::Vec3f t = cv::Vec3f( T.at<int>( cv::Point2d(2,1) ), T.at<int>(0,2), T.at<int>(1,0) );
 
 		// Assure determinant is positive
